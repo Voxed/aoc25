4e3414f29b98ffaf61f2a404ea3720f8c921a806
Petter <petter.blomkvist.pb@gmail.com>
2025-12-01 06:57:12 +0100

    Day 1

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-i = [50] + [int(l[1:]) * (-1 if l[0] == 'L' else 1) for l in open("input.txt").readlines()]
+i = [50] + [int(l[1:]) * (-1 if l[0] == 'L' else 1) for l in open("input.txt")]
 
 # 1
-print(np.count_nonzero(np.cumsum(i) % 100 == 0))
+print(np.sum(np.cumsum(i) % 100 == 0))
 
 # 2
-print(np.count_nonzero(np.cumsum(np.concat([[np.sign(l)] * abs(l) for l in i])) % 100 == 0))
+print(np.sum(np.cumsum(np.concat([[np.sign(l)] * abs(l) for l in i])) % 100 == 0))
