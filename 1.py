2b35b4b517acb051bcacaf183a02cef61f4b05f6
Petter <petter.blomkvist.pb@gmail.com>
2025-12-01 06:53:12 +0100

    Day 1

--- 1.py
+++ 1.py
@@ -1,24 +1,9 @@
 import numpy as np
-import itertools
-#1
-a = [int(l2[1:]) * (-1 if l2[0] == 'L' else 1) for l2 in [l.strip() for l in open("input.txt").readlines()]]
-d = 50
-s = 0
-for l in a:
-    d += l 
-    d = d % 100
-    if d == 0:
-        s += 1
-print(s)
 
-#2
-a = list(itertools.chain(*[[int(np.sign(l3))] * abs(l3) for l3 in a]))
-d = 50
-s = 0
-for l in a:
-    d += l 
-    d = d % 100
-    if d == 0:
-        s += 1
-print(s)
+i = [50] + [int(l[1:]) * (-1 if l[0] == 'L' else 1) for l in open("input.txt").readlines()]
 
+# 1
+print(np.count_nonzero(np.cumsum(i) % 100 == 0))
+
+# 2
+print(np.count_nonzero(np.cumsum(np.concat([[np.sign(l)] * abs(l) for l in i])) % 100 == 0))
