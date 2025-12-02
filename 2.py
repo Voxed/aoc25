291df10bd48933101efa4ac4bcaf8818be4fc768
Petter <petter.blomkvist.pb@gmail.com>
2025-12-02 08:07:20 +0100

    Day 2

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 import re
 l=eval(f"[{re.sub(r"(\d+)-(\d+)",r"*range(\1,\2+1)",open(0).read())}]")
-[print(sum([e for e in l if re.match(r"^(\d+)\1"+r,str(e))]))for r in["$","+$"]]
+[print(sum([e for e in l if re.match(r"(\d+)\1"+r,str(e))]))for r in["$","+$"]]
