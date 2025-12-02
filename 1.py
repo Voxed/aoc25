86466f887d590bf05b3bfd53a247a98bae450365
Petter <petter.blomkvist.pb@gmail.com>
2025-12-02 04:34:56 +0100

    Read inputs from stdin

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-i = [50] + [int(l[1:]) * (-1 if l[0] == 'L' else 1) for l in open("input.txt")]
+i = [int(l[1:]) * (-1 if l[0] == 'L' else 1) for l in open(0)]
 
 # 1
-print(np.sum(np.cumsum(i) % 100 == 0))
+print(np.sum(np.cumsum(i) % 100 == 50))
 
 # 2
-print(np.sum(np.cumsum(np.concat([[np.sign(l)] * abs(l) for l in i])) % 100 == 0))
+print(np.sum(np.cumsum(np.concat([[np.sign(l)] * abs(l) for l in i])) % 100 == 50))
