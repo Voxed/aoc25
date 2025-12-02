d506b9878f7d525fbbd63dc2dd2aaf8b318ebe81
Petter <petter.blomkvist.pb@gmail.com>
2025-12-02 07:40:31 +0100

    Day 2

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 import re
-l=eval('[' + re.sub(r"(\d+)-(\d+)", r"*range(\1,\2)",open(0).read()) + ']')
+l=eval('['+re.sub(r"(\d+)-(\d+)",r"*range(\1,\2)",open(0).read())+']')
 [print(sum([e for e in l if re.match("^(\\d+)\\1"+r,str(e))]))for r in["$","+$"]]
