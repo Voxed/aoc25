409967c10212dc746f95746d7de7a3e2e64aec6a
Petter <petter.blomkvist.pb@gmail.com>
2025-12-02 06:59:02 +0100

    Day 2

--- 2.py
+++ 2.py
@@ -1,6 +1,10 @@
-import numpy as np
 import re
 
-l = np.concat([range(int((f := e.split('-'))[0]), int(f[1])+1) for e in open(0).read().split(',')])
-print(np.sum([e for e in l if re.match("^(\\d+)\\1$", str(e))]))
-print(np.sum([e for e in l if re.match("^(\\d+)\\1+$", str(e))]))
+r = "^(\\d+)\\1"
+l = eval(f"[*range({open(0).read().replace(',','),*range(').replace('-',',')})]")
+
+#1
+print(sum([e for e in l if re.match(r+"$",str(e))]))
+
+#2
+print(sum([e for e in l if re.match(r+"+$",str(e))]))
