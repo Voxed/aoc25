9dfeb1931a8136e1dc42b739016ceb535f14fdac
Petter <petter.blomkvist.pb@gmail.com>
2025-12-02 08:41:47 +0100

    Day 2

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 from re import*
 [i]=open(0)
-[print(sum(e for e in eval(f"[{sub(r"(\d+)-(\d+)",r"*range(\1,\2+1)",i)}]") if match(r"(\d+)\1"+r,str(e))))for r in["$","+$"]]
+[print(sum(e for e in eval(f"[{sub(r"(\d+)-(\d+)",r"*range(\1,\2+1)",i)}]")if match(r"(\d+)\1"+r,str(e))))for r in["$","+$"]]
