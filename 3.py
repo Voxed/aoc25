4b77c862efbaa511d874280f525ef035b6ea320f
Petter <petter.blomkvist.pb@gmail.com>
2025-12-03 07:28:35 +0100

    Day 3

--- 3.py
+++ 3.py
@@ -1,18 +1,11 @@
 import numpy as np
-import re
 
-b0 = [np.array(list(e.strip()), dtype=int) for e in open(0)]
+b = [np.array(list(e.strip()), dtype=int) for e in open(0)]
 
-def largest(b, nums):
-    if nums == 0:
-        return ''
-    if -nums+1 != 0:
-        first = np.argmax(b[:-nums+1])
-    else:
-        first = np.argmax(b)
-    rest = b[first+1:]
-    return str(b[first]) + largest(rest, nums - 1)
+def largest(b, n):
+    if n == -1:
+        return 0
+    i = np.argmax(b[:-n or None])
+    return b[i]*10**n + largest(b[i+1:], n-1)
 
-sum = 0
-for bank in b0:
-    sum += int(largest(bank, 12))
+print(sum([largest(b, 11) for b in b]))
