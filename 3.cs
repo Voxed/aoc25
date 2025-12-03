bb629599189f45143a730c2be2cb1d54b9b034e4
Petter <petter.blomkvist.pb@gmail.com>
2025-12-03 19:13:00 +0100

    Day 3 (csharp)

---
+++ 3.cs
@@ -0,0 +1,10 @@
+List<List<int>> l = new();
+while (Console.ReadLine()?.Select(c=>c-'0').ToList() is List<int> b) l.Add(b);
+double f(List<int> b, int n)
+{
+    if(n < 0) return 0;
+    var m = b[..^n].Max();
+    var i = b.IndexOf(m);
+    return m*Math.Pow(10,n)+f(b[(i+1)..],n-1);
+}
+foreach(var p in new[]{1,11}) Console.WriteLine(l.Select(l=>f(l,p)).Sum());
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 time to burn some lifetime
+
+### Python
+python + powershell
+```powershell
+Get-Content input.txt | python <day>.cs
+```
+
+### C#
+dotnet + powershell
+```powershell
+Get-Content input.txt | dotnet run <day>.cs
+```
