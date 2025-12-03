838578765939ddc72471f8898c0be11ce36b6fc7
Petter <petter.blomkvist.pb@gmail.com>
2025-12-03 08:06:02 +0100

    Day 3

--- 3.py
+++ 3.py
@@ -1,3 +1,3 @@
 from numpy import*
-b,l=[[*map(int,e.strip())]for e in open(0)],lambda b,n:0 if n<0 else b[i:=argmax(b[:len(b)-n])]*10**n+l(b[i+1:],n-1)
+b,l=[[*map(int,e[:-1])]for e in open(0)],lambda b,n:0 if n<0 else b[i:=argmax(b[:len(b)-n])]*10**n+l(b[i+1:],n-1)
 [print(sum([l(b,i)for b in b]))for i in[1,11]]
