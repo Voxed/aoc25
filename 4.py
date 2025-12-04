b217fd0e095bb78d1dae37199c10e95b5b2431ec
Petter <petter.blomkvist.pb@gmail.com>
2025-12-04 07:37:34 +0100

    Day 4

--- 4.py
+++ 4.py
@@ -1,16 +1,37 @@
 import numpy as np
-f = 1
-l = np.pad(np.array([[1 if u == '@' else 0 for u in e.strip()] for e in open(0)]), ((1,1),(1,1)))
-a = 0
+
+answer = 0
+
+# Parse world with a padding of empty space to ensure that each scroll can look in a 3x3 vicinity.
+# '.' = 0
+# '@' = 1
+world = np.pad(np.array([[u == '@' for u in e.strip()] for e in open(0)], dtype=int), 1)
+
+# Remove until nothing to remove
 while True:
-    s = np.roll(l, 1, 0) + l + np.roll(l, -1, 0)
-    t = np.roll(s, 1, 1) + s + np.roll(s, -1, 1)
-    r = (l==1) & (t<5)
-    a += r.sum()
-    if f:
-        f = 0
-        print(a)
-    if r.sum() == 0:
+
+    # 3x3 Kernel (Each element will contain scrolls in 3x3 vicinity)
+    s = np.roll(world, 1, 0) + world + np.roll(world, -1, 0) # Vertical kernel
+    scroll_count = np.roll(s, 1, 1) + s + np.roll(s, -1, 1)  # Horizontal kernel
+
+    # Removal condition
+    remove = (world == 1) & (scroll_count < 5)
+
+    is_p1 = answer == 0
+
+    # Add removals to tally
+    answer += remove.sum()
+
+    # Print answer to p1
+    if is_p1:
+        print(answer)
+
+    # Break if nothing to remove
+    if not remove.sum():
         break
-    l = l & ~r
-print(a)
+
+    # Remove free scrolls
+    world = world & ~remove
+
+# Print answer to p2
+print(answer)
