f72d56df002baf08d19655834fdb98c41121ccd2
Petter <petter.blomkvist.pb@gmail.com>
2025-12-04 07:08:08 +0100

    Day 4

--- 4.py
+++ 4.py
@@ -1,36 +1,16 @@
-import numpy as npagt
-
-l = [list(e.strip()) for e in open(0)]
-sum = 0
-while(True):
-    for y, r in enumerate(l):
-        for x, e in enumerate(r):
-            s = 0
-            if y > 0:
-                if l[y-1][x] == '@':
-                    s+=1
-                if x < len(r)-1:
-                    if l[y-1][x+1] == '@':
-                        s+=1
-                if x > 0:
-                    if l[y-1][x-1] == '@':
-                        s+=1
-            if y < len(l)-1:
-                if l[y+1][x] == '@':
-                    s+=1
-                if x < len(r)-1:
-                    if l[y+1][x+1] == '@':
-                        s+=1
-                if x > 0:
-                    if l[y+1][x-1] == '@':
-                        s+=1
-            if x > 0:
-                if l[y][x-1] == '@':
-                    s+=1
-            if x < len(r)-1:
-                if l[y][x+1] == '@':
-                    s+=1
-            if s < 4 and e == '@':
-                l[y][x] = '.'
-                sum += 1
-    print(sum) # This will start spamming the same number when stagnated :)
+import numpy as np
+f = 1
+l = np.pad(np.array([[1 if u == '@' else 0 for u in e.strip()] for e in open(0)]), ((1,1),(1,1)))
+a = 0
+while True:
+    s = np.roll(l, 1, 0) + l + np.roll(l, -1, 0)
+    t = np.roll(s, 1, 1) + s + np.roll(s, -1, 1)
+    r = (l==1) & (t<5)
+    a += r.sum()
+    if f:
+        f = 0
+        print(a)
+    if r.sum() == 0:
+        break
+    l = l & ~r
+print(a)
