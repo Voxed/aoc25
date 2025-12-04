395441d1334b2122ad28c2b25c813c49fa282b06
Petter <petter.blomkvist.pb@gmail.com>
2025-12-04 07:44:33 +0100

    Day 4

--- 4.py
+++ 4.py
@@ -5,17 +5,17 @@ answer = 0
 # Parse world with a padding of empty space to ensure that each scroll can look in a 3x3 vicinity.
 # '.' = 0
 # '@' = 1
-world = np.pad(np.array([[u == '@' for u in e.strip()] for e in open(0)], dtype=int), 1)
+has_scroll = np.pad(np.array([[u == '@' for u in e.strip()] for e in open(0)], dtype=int), 1)
 
 # Remove until nothing to remove
 while True:
 
     # 3x3 Kernel (Each element will contain scrolls in 3x3 vicinity)
-    s = np.roll(world, 1, 0) + world + np.roll(world, -1, 0) # Vertical kernel
-    scroll_count = np.roll(s, 1, 1) + s + np.roll(s, -1, 1)  # Horizontal kernel
+    vert = np.roll(has_scroll, 1, 0) + has_scroll + np.roll(has_scroll, -1, 0) # Vertical kernel
+    scroll_count = np.roll(vert, 1, 1) + vert + np.roll(vert, -1, 1)           # Horizontal kernel
 
-    # Removal condition
-    remove = (world == 1) & (scroll_count < 5)
+    # Removal condition (Scroll count in a 3x3 vicinity is actually max 4 if you count resident scroll)
+    remove = has_scroll & (scroll_count <= 4)
 
     is_p1 = answer == 0
 
@@ -31,7 +31,7 @@ while True:
         break
 
     # Remove free scrolls
-    world = world & ~remove
+    has_scroll = has_scroll & ~remove
 
 # Print answer to p2
 print(answer)
