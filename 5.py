7774dfb03c65b2332b687c04657b28c79c69e0ff
Petter <petter.blomkvist.pb@gmail.com>
2025-12-05 07:29:16 +0100

    Day 5

--- 5.py
+++ 5.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-lines = [e.strip() for e in open(0)]
+ranges, ingredients = open(0).read().split('\n\n')
 
 # Parse ranges into tuples of (start(inclusive), end(exclusive)).
 ranges = [tuple(np.array(e.split('-'), dtype=int)+(0, 1))
-          for e in lines[:lines.index('')]]
+          for e in ranges.split()]
 # Sort based on range start, this is the magic for Part 2
 ranges.sort()
 
 # Part 1, straightforward add if ingredient is in any range.
 a = 0
-for ingredient in [int(e) for e in lines[lines.index('')+1:]]:
+for ingredient in [int(e) for e in ingredients.split()]:
     for r in ranges:
         if r[0] <= ingredient < r[1]:
             a += 1
