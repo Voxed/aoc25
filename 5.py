246511709feed1d624ba95888cf4bf279434468c
Petter <petter.blomkvist.pb@gmail.com>
2025-12-05 06:57:07 +0100

    Day 5

--- 5.py
+++ 5.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 lines = [e.strip() for e in open(0)]
+
+# Parse ranges into tuples of (start(inclusive), end(exclusive)).
 ranges = [tuple(np.array(e.split('-'), dtype=int)+(0, 1))
           for e in lines[:lines.index('')]]
+# Sort based on range start, this is the magic for Part 2
 ranges.sort()
 
-# Part 1
+# Part 1, straightforward add if ingredient is in any range.
 a = 0
 for ingredient in [int(e) for e in lines[lines.index('')+1:]]:
     for r in ranges:
@@ -17,9 +20,15 @@ print(a)
 # Part 2
 a = 0
 cursor = 0
-for r in ranges:
-    if cursor >= r[1]:
+for (start, end) in ranges:
+    # If the cursor has passed the end of the range, it was contained
+    # within some other range.
+    if cursor >= end:
         continue
-    a += min(r[1]-cursor, r[1]-r[0])
-    cursor = r[1]
+    # Since the cursor can be located before the range start, we need to
+    # cap the size of the fresh range.
+    a += min(end-cursor, end-start)
+    # Since our ranges are sorted, we can move the cursor to the end
+    # without skipping any earlier ranges.
+    cursor = end
 print(a)
