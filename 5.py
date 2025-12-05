52831bf746aab6381107bb7742f5fc32af041f58
Petter <petter.blomkvist.pb@gmail.com>
2025-12-05 06:47:37 +0100

    Day 5

--- 5.py
+++ 5.py
@@ -1,12 +1,15 @@
+import numpy as np
+
 lines = [e.strip() for e in open(0)]
-ranges=[[*map(int,e.split('-'))] for e in lines[:lines.index('')]]
+ranges = [tuple(np.array(e.split('-'), dtype=int)+(0, 1))
+          for e in lines[:lines.index('')]]
 ranges.sort()
 
 # Part 1
 a = 0
 for ingredient in [int(e) for e in lines[lines.index('')+1:]]:
     for r in ranges:
-        if ingredient >= r[0] and ingredient <= r[1]:
+        if r[0] <= ingredient < r[1]:
             a += 1
             break
 print(a)
@@ -15,10 +18,8 @@ print(a)
 a = 0
 cursor = 0
 for r in ranges:
-    if cursor < r[0]:
-        cursor = r[0]
-    if cursor > r[1]:
+    if cursor >= r[1]:
         continue
-    a += r[1]+1 - cursor
-    cursor = r[1]+1
+    a += min(r[1]-cursor, r[1]-r[0])
+    cursor = r[1]
 print(a)
