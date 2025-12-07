cb7ed0367173cebd7344d7b786f6e7c10311b65e
Petter <petter.blomkvist.pb@gmail.com>
2025-12-07 06:38:52 +0100

    Day 7

--- 07.py
+++ 07.py
@@ -1,7 +1,7 @@
-import copy
 from functools import cache
 
 l = [list(e.strip()) for e in open(0)]
+reached = set()
 
 @cache
 def tl(x, y):
@@ -10,11 +10,15 @@ def tl(x, y):
             case '.':
                 return tl(x,y+1)
             case '^':
+                reached.add((x,y+1))
                 return tl(x+1,y+1) + tl(x-1,y+1) + 1
     return 0
 
+timelines = 0
 for y, r in enumerate(l):
     for x, e in enumerate(r):
         if e == 'S':
-            print("S at ", x, y)
-            print(tl(x,y)+1, x, y)
+            timelines = tl(x,y)+1
+
+print(timelines)
+print(len(reached))
