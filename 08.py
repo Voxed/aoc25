15e7145cce162d73edc3a9d9e13020f6f824f8bf
Petter <petter.blomkvist.pb@gmail.com>
2025-12-08 07:51:13 +0100

    Day 8

--- 08.py
+++ 08.py
@@ -1,35 +1,34 @@
 import numpy as np
-from itertools import product
+from itertools import combinations
 import math
 import networkx as nx
+from networkx.utils import UnionFind
 
 l = [list(map(int,e.strip().split(','))) for e in open(0)]
 
-smallest_pairs = []
-for i, (x1,y1,z1) in enumerate(l):
-    for j, (x2,y2,z2) in enumerate(l):
-        if i != j:
-            di = math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2,2) + math.pow(z1-z2,2))
-            smallest_pairs.append((di, i, j))
-smallest_pairs.sort()
+p1_connections = 10 if len(l) < 1000 else 1000
 
+nodes = range(0, len(l))
+indices = combinations(nodes, 2)
+distances = map(lambda p: math.dist(*p), combinations(l, 2))
+
+pairs = list(zip(distances, indices))
+pairs.sort()
+
+U = UnionFind(nodes)
 G = nx.Graph()
-
-units = {}
-nunits = 0
-connections = 0
-for i, j in enumerate(l):
-    G.add_node(i)
-for (d, p1, p2) in smallest_pairs:
+num_components = len(nodes)
+for n in nodes:
+    G.add_node(n)
+for (d, (p1, p2)) in pairs:
     if p2 not in nx.neighbors(G, p1):
         G.add_edge(p1, p2)
-        connections += 1
-        if sorted(map(len, nx.connected_components(G)), reverse=True)[0] == 1000:
-            print(l[p1][0]* l[p2][0])
-            break
-
-sizes = list(map(len, list(nx.connected_components(G))))
-sizes.sort()
-sizes = list(reversed(sizes))
-print(sizes)
-print(sizes[0] * sizes[1] * sizes[2])
+        if len(G.edges) == p1_connections:
+            components = sorted(map(len, U.to_sets()), reverse=True)
+            print(np.prod(components[0:3]))
+        if U[p1] != U[p2]:
+            num_components -= 1
+            U.union(p1, p2)
+            if num_components == 1:
+                print(l[p1][0] * l[p2][0])
+                break
