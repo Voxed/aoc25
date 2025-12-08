51e817065e36baa8a9e73811baac5f71b4c2df3f
Petter <petter.blomkvist.pb@gmail.com>
2025-12-08 08:34:10 +0100

    Day 8

--- 08.py
+++ 08.py
@@ -1,22 +1,35 @@
 from itertools import combinations
-import math
+from math import dist, prod
 from networkx.utils import UnionFind
 
 pos = [eval(e) for e in open(0)]
+
+# I don't like questions where you have to do this
 p1_connections = 10 if len(pos) < 1000 else 1000
+
 nodes = range(0, len(pos))
+
+# Sorted pairs of form [(dist, index1, index2)]
 pairs = sorted(zip(
-    map(lambda p: math.dist(*p), combinations(pos, 2)),
+    map(lambda p: dist(*p), combinations(pos, 2)),
     combinations(nodes, 2)))
 
 U = UnionFind(nodes)
-num_components = len(nodes)
+# We start out with N disjunct sets
+num_sets = len(nodes)
+
+# Join edges
 for edges, (d, p) in enumerate(pairs):
+    # If the amount of edges reaches p1_connections,
+    # we print the product of the length of the 3 largest sets.
     if edges == p1_connections:
-        print(math.prod(sorted(map(len, U.to_sets()), reverse=True)[0:3]))
+        print(prod(sorted(map(len, U.to_sets()), reverse=True)[0:3]))
+
+    # If the junctions are not in the same set, join them
     if U[p[0]] != U[p[1]]:
-        num_components -= 1
+        num_sets -= 1
         U.union(*p)
-        if num_components == 1:
+        # When the number of sets reach 1, we have joined everything
+        if num_sets == 1:
             print(pos[p[0]][0] * pos[p[1]][0])
             break
