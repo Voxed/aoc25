0b069641066e04d7975a9f9cb3f179780a961089
Petter <petter.blomkvist.pb@gmail.com>
2025-12-10 10:10:39 +0100

    Day 10

--- 10.py
+++ 10.py
@@ -1,18 +1,37 @@
 from collections import namedtuple
 import numpy as np
 from scipy.optimize import milp, LinearConstraint, Bounds
+from itertools import count
 
 Machine = namedtuple('Machine', ('indicators', 'buttons', 'joltage'))
 machines = []
 
 l = [e.strip().split(' ') for e in open(0)]
 for m in l:
-    indicators = [e == '#' for e in m[0][1:-1]]
+    indicators = tuple(e == '#' for e in m[0][1:-1])
     buttons = [eval(e.replace(')', ',)')) for e in m[1:-1]]
     joltages = list(map(int, m[-1][1:-1].split(',')))
     machines.append(Machine(indicators, buttons, joltages))
 
-ss = 0
+part1 = 0
+for m in machines:
+    states = set([tuple(False for _ in m.indicators)])
+    for i in count(1):
+        found_solution = False
+        new_states = set()
+        for state in states:
+            for button in m.buttons:
+                new_state = tuple(indicator ^ (i in button) for i, indicator in enumerate(state))
+                new_states.add(new_state)
+                if new_state == m.indicators:
+                    found_solution = True
+        if found_solution:
+            break
+        states = new_states
+    part1 += i
+print(part1)
+
+part2 = 0
 for m in machines:
     eqs = [
         [1 if i in b else 0 for b in m.buttons]
@@ -24,6 +43,5 @@ for m in machines:
                 bounds=Bounds(0, np.inf), 
                 integrality=np.ones(num_variables, dtype=int))
 
-    ss += result.fun
-
-print(int(ss))
+    part2 += result.fun
+print(int(part2))
