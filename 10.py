8f75ad236da0bc6df03dd516b03fb1e1684aeb5f
Petter <petter.blomkvist.pb@gmail.com>
2025-12-10 12:02:37 +0100

    Day 10

--- 10.py
+++ 10.py
@@ -1,49 +1,53 @@
-from collections import namedtuple
 import numpy as np
 from scipy.optimize import milp, LinearConstraint, Bounds
-from itertools import count
+from itertools import product, count
 
-Machine = namedtuple('Machine', ('indicators', 'buttons', 'joltage'))
-machines = []
+# Parse the machines
+machines = [
+    (
+        # Parse indicator requirements
+        tuple(e == '#' for e in m[0][1:-1]),
 
-l = [e.strip().split(' ') for e in open(0)]
-for m in l:
-    indicators = tuple(e == '#' for e in m[0][1:-1])
-    buttons = [eval(e.replace(')', ',)')) for e in m[1:-1]]
-    joltages = list(map(int, m[-1][1:-1].split(',')))
-    machines.append(Machine(indicators, buttons, joltages))
+        # Parse buttons
+        [eval(e.replace(')', ',)')) for e in m[1:-1]],
+
+        # Parse joltage requirements
+        list(map(int, m[-1][1:-1].split(',')))
+    )
+    for m in [e.strip().split(' ') for e in open(0)]]
 
 # Breadth first search
 part1 = 0
-for m in machines:
-    states = set([tuple(False for _ in m.indicators)])
+for (req_indicators, buttons, _) in machines:
+    states = set([tuple(False for _ in req_indicators)])
     for i in count(1):
-        found_solution = False
         new_states = set()
-        for state in states:
-            for button in m.buttons:
-                new_state = tuple(indicator ^ (i in button) for i, indicator in enumerate(state))
-                new_states.add(new_state)
-                if new_state == m.indicators:
-                    found_solution = True
-        if found_solution:
-            break
-        states = new_states
+        for state, button in product(states, buttons):
+            new_state = tuple(
+                indicator ^ (i in button)
+                for i, indicator in enumerate(state)
+            )
+            new_states.add(new_state)
+            if new_state == req_indicators:
+                break
+        else:
+            states = new_states
+            continue
+        break
     part1 += i
 print(part1)
 
 # Mixed integer linear programming
 part2 = 0
-for m in machines:
+for (_, buttons, joltage_req) in machines:
     eqs = [
-        [1 if i in b else 0 for b in m.buttons]
-        for i in range(len(m.joltage))
+        [1 if i in b else 0 for b in buttons]
+        for i in range(len(joltage_req))
     ]
-    num_variables = len(m.buttons)
-    result = milp(c=np.ones(num_variables), 
-                constraints=LinearConstraint(eqs, m.joltage, m.joltage), # m.joltage <= coefficients * variables <= m.joltage
-                bounds=Bounds(0, np.inf), 
-                integrality=np.ones(num_variables, dtype=int))
-
+    num_variables = len(buttons)
+    result = milp(c=np.ones(num_variables),
+                  constraints=LinearConstraint(eqs, joltage_req, joltage_req),
+                  bounds=Bounds(0, np.inf),
+                  integrality=np.ones(num_variables, dtype=int))
     part2 += result.fun
 print(int(part2))
