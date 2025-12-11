90eac39a46ee0fd3fbf5e16b5e29ca601a7a2adc
Petter <petter.blomkvist.pb@gmail.com>
2025-12-11 07:34:23 +0100

    Day 11

--- 11.py
+++ 11.py
@@ -1,35 +1,29 @@
-import networkx as nx
 from functools import cache
 
-l = [
-    e.strip()
-    for e in open(0)
-]
+# Parse graph into dictionary of node -> [node]
+G = {
+    e[0]: e[1].split()
+    for e in [e.split(': ') for e in open(0)]
+}
 
-G = nx.DiGraph()
-for r in l:
-    G.add_node(r.split(':')[0])
+# Number of paths from f -> t
+def P(f, t):
+    @cache
+    def r(f):
+        return 1 if f == t else sum(map(r, G.get(f, [])))
+    return r(f)
 
-for r in l:
-    n = r.split(':')[0]
-    others = r.split(':')[1][1:].split()
-    for o in others:
-        G.add_edge(n, o)
+# Part 1
+print(P('you', 'out'))
 
-@cache
-def num_paths(f, t):
-    if f == t:
-        return 1
-    count = 0
-    for v in G.neighbors(f):
-        count += num_paths(v, t)
-    return count
-
-svr_to_dac = num_paths('svr', 'dac')
-svr_to_fft = num_paths('svr', 'fft')
-dac_to_fft = num_paths('dac', 'fft')
-fft_to_dac = num_paths('fft', 'dac')
-dac_to_out = num_paths('dac', 'out')
-fft_to_out = num_paths('fft', 'out')
-
-print(svr_to_dac * dac_to_fft * fft_to_out + svr_to_fft * fft_to_dac * dac_to_out)
+# Part 2
+print(
+    # svr -> dac -> fft -> out
+    P('svr', 'dac') *
+    P('dac', 'fft') *
+    P('fft', 'out') +
+    # svr -> fft -> dac -> out
+    P('svr', 'fft') *
+    P('fft', 'dac') *
+    P('dac', 'out')
+)
