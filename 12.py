7f98797f15c8a39428e6aa9219c6e18938c6b51d
Petter <petter.blomkvist.pb@gmail.com>
2025-12-12 09:25:05 +0100

    Day 12

--- 12.py
+++ 12.py
@@ -1,4 +1,3 @@
 cond = [(list(map(int, a.split('x'))), list(map(int, b.split())))
         for a, b in [e.split(': ') for e in open(0).read().split('\n\n')[-1].strip().split('\n')]]
-
 print(sum([sum(c) * 9 <= x * y for (x, y), c in cond]))
