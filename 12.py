858a1b1773a9840d0f37fd20e4bbc10fb4cef9ee
Petter <petter.blomkvist.pb@gmail.com>
2025-12-12 09:14:42 +0100

    Day 12

--- 12.py
+++ 12.py
@@ -1,7 +1,4 @@
 cond = [(list(map(int, a.split('x'))), list(map(int, b.split())))
         for a, b in [e.split(': ') for e in open(0).read().split('\n\n')[-1].strip().split('\n')]]
 
-# This only works in the input. Had a full set packing approach that got stuck on ex with this
-# prune. Turns out input works just fine. Yikes.
-# Probably extend this with some actual set packing for the two failed example conditions.
 print(sum([sum(c) * 9 <= x * y for (x, y), c in cond]))
